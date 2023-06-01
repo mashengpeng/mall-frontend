6d4f2ce6c62dd576acdc34b02b514ae71cc8213b
shengpeng.ma <shengpeng.ma@nio.com>
2023-06-01 20:20:44 +0800

    购物车列表优化

--- src/views/CartListView.vue
+++ src/views/CartListView.vue
@@ -10,11 +10,14 @@
         <el-card v-for="item in data.items" :key="item.skuId">
           <template #header>
             <div class="title">
-              <el-link :underline="false" @click="toDetail(item.skuId)">{{
-                item.title
-              }}</el-link>
-
-              <el-switch v-model="item.check" @click="check"></el-switch>
+              <el-link :underline="false" @click="toDetail(item.skuId)"
+                >{{ item.title }}
+              </el-link>
+              <el-checkbox
+                v-model="item.check"
+                size="large"
+                @click.prevent="check(item.skuId, !item.check)"
+              ></el-checkbox>
             </div>
           </template>
           <div class="content">
@@ -23,33 +26,56 @@
               :preview-src-list="[item.image]"
               :src="item.image"
             ></el-image>
-            <el-descriptions
-              :column="4"
-              border
-              direction="vertical"
-              title="商品信息"
-            >
-              <el-descriptions-item label="价格"
+            <el-descriptions :column="10" border direction="vertical">
+              <el-descriptions-item align="center" label="价格"
                 >{{ item.price }}
               </el-descriptions-item>
-              <el-descriptions-item label="总价格"
+              <el-descriptions-item align="center" label="数量">
+                <el-input-number
+                  v-model="item.count"
+                  controls-position="right"
+                  label="数量"
+                  min="0"
+                  @change="changeNum(item.skuId, item.count)"
+                ></el-input-number>
+              </el-descriptions-item>
+              <el-descriptions-item align="center" label="总价格"
                 >{{ item.totalPrice }}
               </el-descriptions-item>
               <el-descriptions-item
                 v-for="(attr, index) in item.skuAttrValues"
                 :key="index"
                 :label="attr.split('：')[0]"
+                align="center"
                 >{{ attr.split("：")[1] }}
               </el-descriptions-item>
             </el-descriptions>
-            <el-input-number
-              v-model="item.count"
-              label="数量"
-              @change="handleChange"
-            ></el-input-number>
           </div>
         </el-card>
       </el-space>
+      <el-divider></el-divider>
+      <el-descriptions :column="2" border title="总览">
+        <template #extra>
+          <el-button size="large" type="primary">结算</el-button>
+        </template>
+        <el-descriptions-item>
+          <template #label> 商品总数</template>
+          {{ data.countNum + " 件" }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label> 商品种数</template>
+          {{ data.countType }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label> 总价</template>
+          {{ data.totalAmount + " 元" }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label> 折扣</template>
+          {{ data.reduce }}
+        </el-descriptions-item>
+      </el-descriptions>
+      <el-backtop target=".el-main"></el-backtop>
     </div>
   </div>
 </template>
@@ -58,6 +84,7 @@
 import { ref } from "vue";
 import myAxios from "@/utils/httpRequest";
 import router from "@/router";
+import { ElNotification } from "element-plus";
 
 let isReady = ref(false);
 
@@ -69,8 +96,42 @@ const toDetail = (skuId) => {
   });
 };
 
-const check = () => {
-  console.log(data);
+const check = (skuId, checked) => {
+  myAxios
+    .post("/cart/check", null, { params: { skuId, checked: checked ? 1 : 0 } })
+    .then(
+      (res) => {
+        ElNotification({
+          title: "操作成功",
+          type: "success",
+          position: "top-left",
+        });
+        data.value = res.data;
+      },
+      () => {}
+    );
+};
+const changeNum = (skuId, num) => {
+  myAxios.post("/cart/changeNum", null, { params: { skuId, num } }).then(
+    (res) => {
+      if (num === 0) {
+        ElNotification({
+          title: "已删除商品",
+          type: "info",
+          position: "top-left",
+        });
+      } else {
+        ElNotification({
+          title: "操作成功",
+          type: "success",
+          position: "top-left",
+        });
+      }
+
+      data.value = res.data;
+    },
+    () => {}
+  );
 };
 
 const loadData = () => {
